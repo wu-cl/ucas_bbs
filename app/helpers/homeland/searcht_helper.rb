8d1a92fdaa93ed728c2d37b8d4d8f3e99f6f0d66
tymrail <tymrail@163.com>
2019-01-24 00:25:18 -0800

    change

--- app/helpers/homeland/searcht_helper.rb
+++ app/helpers/homeland/searcht_helper.rb
@@ -1,4 +1,8 @@
 module Homeland
   module SearchtHelper
+    def render_highlight_content(topic,query_string)
+      excerpt_cont = excerpt(topic.title, query_string, radius: 500)
+      highlight(excerpt_cont, query_string)
+    end
   end
 end
